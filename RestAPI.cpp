c02250573e9536092db883392d51277f4fef0252
Mohammad <mhn1999@gmail.com>
2024-12-08 23:25:37 +0000

    added the CMakeLists.txt to the project

---
+++ RestAPI.cpp
@@ -0,0 +1,12 @@
+//
+// Created by mhn on 12/8/24.
+//
+
+#include "RestAPI.h"
+
+using namespace API;
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
---
+++ RestAPI.h
@@ -0,0 +1,17 @@
+//
+// Created by mhn on 12/8/24.
+//
+
+#ifndef RESTAPI_H
+#define RESTAPI_H
+
+
+namespace API {
+class Restful {
+  public:
+    void readConfig();
+};
+}
+
+
+#endif //RESTAPI_H
